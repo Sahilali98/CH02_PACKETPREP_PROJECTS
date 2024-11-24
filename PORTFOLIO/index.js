3c435025844877b5b6c89f745fd34fafaa26e92a
SK SAHIL ALI <sahilali.ind@gmail.com>
2024-11-24 11:08:44 +0530

    translator app added

---
+++ PORTFOLIO/index.js
@@ -0,0 +1,23 @@
+let receiverEmail = document.getElementById('fromEmail').value;
+let subject = document.getElementById('subject').value;
+let body = document.getElementById('body').value;
+let form = document.getElementById('form');
+form.addEventListener('submit',sendEmail);
+function sendEmail(event) {
+    event.preventDefault();
+    Email.send({
+        Host: "smtp.gmail.com",
+        Server: 'smtp.elasticemail.com',
+        Port: '2525',
+        Username: "sksahilali.ind@gmail.com",
+        Password: "C79003E441C384F0317378623667B6B83DBC",
+        To: receiverEmail,
+        From: "sksahilali.ind@gmail.com",
+        Subject: subject,
+        Body: body,
+    })
+        .then(function (message) {
+            console.log(message);
+            alert("mail sent successfully")
+        });
+}
